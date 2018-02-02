c68bccc2ba971c5369a432cf13687b87ee7f21ff
VadimKh <khoroshiltsev.vadim@gmail.com>
2018-02-02 14:13:55 +0300

    Add signup form (initial)

---
+++ src/lib/SignIn.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Card } from 'reactstrap';
+
+export default function SignInForm() {
+    return (
+        <Card className="card-signup">
+            TEXET
+        </Card>
+    );
+}
