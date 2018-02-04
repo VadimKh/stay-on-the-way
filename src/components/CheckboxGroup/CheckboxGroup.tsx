c3839c91059572b28f7214757e115fd7ef06e356
VadimKh <khoroshiltsev.vadim@gmail.com>
2018-02-04 12:39:16 +0300

    Add checkbox

---
+++ src/components/CheckboxGroup/CheckboxGroup.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import './CheckboxGroup.scss';
+
+export default function(props: ICheckboxGroupProps) {
+    return <span className="bmd-form-group">
+        <div className="input-group">
+            <div className="form-check">
+                <label className="form-check-label">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value={props.value}
+                        onChange={props.onChange}
+                    />
+                    {props.title}
+                    <span className="form-check-sign">
+                        <span className="check"></span>
+                    </span>
+                </label>
+            </div>
+        </div>
+    </span>
+}
+
+export interface ICheckboxGroupProps {
+    title?: string,
+    onChange?: VoidFunction,
+    value?: string
+}
